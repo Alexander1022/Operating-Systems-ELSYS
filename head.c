17cb972f798ace81b72c162802cda6fa5d12ffad
Alexander <36764735+Alexander1022@users.noreply.github.com>
2020-11-06 12:32:20 +0200

    maybe a little update .. idk

--- head.c
+++ head.c
@@ -9,49 +9,42 @@
 int main()
 {
     int file = 0;
-    size_t size = 4096;
+    int res = 0;
+    int offset = 0;
+    size_t size = 100000;
     char *copy_buff = malloc(size);
-    size_t offset = 0;
-    size_t res;
-    
-    if((file = open("a.txt", O_RDONLY)) < 0)
+
+    file = open("a.txt", O_RDONLY);
+
+    if(file < 0)
     {
-        write(1, "Invalid argument", 16);
+        perror("Error");
     }
 
     else
     {
-        file = open("a.txt", O_RDONLY);
         int j = 0;
-        
-        while((res = read(file, copy_buff + offset, 100000)) > 0) 
-        {
-                offset += res;
-                if (offset + 100000 > size) 
-                {
-                        size *= 2;
-                        copy_buff = realloc(copy_buff, size);
-                }
-        }
-        
-        //read(file, copy_buffer, 1999999999);
-        
         int i = 0;
         int counter = 0;
-        
-        while(counter <10)
+        while((res = read(file, copy_buff + offset, 100000)) > 0 || counter < 10)
         {
-		write(STDOUT_FILENO, (copy_buff + i), 1);
-		i++;
-		if(copy_buff[i] == '\n')
-		{
-			counter ++;
-		}
+            write(STDOUT_FILENO, (copy_buff + i), 1);
+            i++;
+            if(copy_buff[i] == '\n')
+            {
+              counter ++;
+            }
+            offset = offset + res;
+            if(offset + 100000 > size)
+            {
+              size = size * 2;
+              copy_buff = realloc(copy_buff, size);
+            }
         }
-        
+
         copy_buff[size - 1] = '\0';
         write(STDOUT_FILENO, "\n", 1);
-        
+
         close(file);
     }
     return 0;
