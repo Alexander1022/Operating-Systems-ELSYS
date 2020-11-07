2dd9102d3c7ba9b8bb2cd405ae0a3cde8c91d39f
Alexander <36764735+Alexander1022@users.noreply.github.com>
2020-11-07 21:41:14 +0200

    updated

--- headers.c
+++ headers.c
@@ -10,109 +10,108 @@ int main(int argc, char *argv[])
 {
     if(argc == 2)
     {
-    	    int file = 0;
-	    size_t size = 4096;
-	    char buffer[size];
-	    size_t n;
+        	int file = 0;
+    	    size_t size = 4096;
+    	    char buffer[size];
+    	    size_t n;
 
 	    file = open(argv[1], O_RDONLY);
-	    
+
 	    if(file < 0)
 	    {
-		perror("Error");
+		      perror("Error");
 	    }
 
 	    else
 	    {
-		int i;
-		int j = 0;
-		int counter = 0;
-		int counter_2 = 0;
+      		int i;
+      		int j = 0;
+      		int counter = 0;
+      		int counter_2 = 0;
 
-		while((n=read(file, buffer, 4096)) > 0) 
-	       	{
-		 	for(i = 0 ; i < n && counter < 10 ; i++)
-		 	{
-		 		if(buffer[i] == '\n')
-		 		{
-		 			counter ++;
-		 		}
-		 	}
-		 	
-		 	if(counter == 10) 
-		 	{
-		 		write(1, buffer, i);
-		 		break; 
-		 	}
-	   
-		 	else
-		 	{
-		 		write(1, buffer, n);
-		 	}
-		 	
-		}
+		        while((n=read(file, buffer, 4096)) > 0)
+	       	  {
+    		 	      for(i = 0 ; i < n && counter < 10 ; i++)
+    		 	      {
+    		 		         if(buffer[i] == '\n')
+    		 		         {
+    		 			             counter ++;
+    		 		         }
+    		 	      }
+
+          		 	if(counter == 10)
+          		 	{
+          		 		   write(1, buffer, i);
+          		 		   break;
+          		 	}
+
+          		 	else
+          		 	{
+          		 		   write(1, buffer, n);
+          		 	}
+		         }
 
 		write(1, "\n", 1);
 		close(file);
-	    }
-    }
+
+     }
+   }
     else
     {
-    for(int o = 1 ; o < argc ; o++)
-    {
+      for(int o = 1 ; o < argc ; o++)
+      {
             int file = 0;
-	    size_t size = 4096;
-	    char buffer[size];
-	    size_t n;
+      	    size_t size = 4096;
+      	    char buffer[size];
+      	    size_t n;
 
-	    file = open(argv[o], O_RDONLY);
-	    int name_count = sizeof(argv[0]);
-	    
-	    if(file < 0)
-	    {
-		perror("Error");
-	    }
+      	    file = open(argv[o], O_RDONLY);
+      	    int name_count = sizeof(argv[o]) - 2;
 
-	    else
-	    {
-	    	write(1, "==> ", 4);
-	    	write(1, argv[o], name_count);
-	    	write(1, " <==", 4);
-	    	write(1, "\n", 1);
-		int i;
-		int j = 0;
-		int counter = 0;
-		int counter_2 = 0;
+      	    if(file < 0)
+      	    {
+      		      perror("Error");
+      	    }
 
-		while((n=read(file, buffer, 4096)) > 0) 
-	       	{
-		 	for(i = 0 ; i < n && counter < 10 ; i++)
-		 	{
-		 		if(buffer[i] == '\n')
-		 		{
-		 			counter ++;
-		 		}
-		 	}
-		 	
-		 	if(counter == 10) 
-		 	{
-		 		write(1, buffer, i);
-		 		break; 
-		 	}
-	   
-		 	else
-		 	{
-		 		write(1, buffer, n);
-		 	}
-		 	
-		}
+	           else
+	            {
+        	    	write(1, "==> ", 4);
+        	    	write(1, argv[o], name_count);
+        	    	write(1, " <==", 4);
+        	    	write(1, "\n", 1);
+            		int i;
+            		int j = 0;
+            		int counter = 0;
+            		int counter_2 = 0;
 
-		write(1, "\n", 1);
-		close(file);
-	    }
+		            while((n=read(file, buffer, 4096)) > 0)
+	       	       {
+		 	               for(i = 0 ; i < n && counter < 10 ; i++)
+		 	                 {
+		 		                    if(buffer[i] == '\n')
+		 		                       {
+		 			                           counter ++;
+		 		                       }
+		 	                 }
+
+                		 	if(counter == 10)
+                		 	{
+                		 		write(1, buffer, i);
+                		 		break;
+                		 	}
+
+                		 	else
+                		 	{
+                		 		write(1, buffer, n);
+                		 	}
+
+		              }
+
+      		write(1, "\n", 1);
+      		close(file);
+	       }
+       }
    }
-   }
-    
-    
+   	write(1, "\n", 1);
     return 0;
 }
