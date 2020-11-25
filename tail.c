72fc11244ae9e2738410cd84d20f16975ea5d712
Alexander <36764735+Alexander1022@users.noreply.github.com>
2020-11-25 16:23:01 +0200

    more than 2 argc updated
    more than 2 argc updated

--- tail.c
+++ tail.c
@@ -5,15 +5,84 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
 	if(argc < 2)
 	{
-		
+		write(1, "You have to write something here!", 33);
+	} 
+
+	else if(argc > 2)
+	{
+		for(int f = 1 ; f < argc ; f++)
+		{
+			int name_count = strlen(argv[f]);
+			write(1, "==> ", 4);
+			write(1, argv[f], name_count);
+			write(1, " <==", 4);
+			write(1, "\n", 1);
+			int file = 0;
+			file = open(argv[f], O_RDONLY);
+			char wow;
+			size_t n;
+			int counter = 0;
+			int characters = 0;
+			int i = 0;
+
+			if(file < 0)
+			{
+				return -1;
+			}
+
+			else
+			{	
+				i = lseek(file, 0, SEEK_END);
+
+				while(i != -1 && counter != 10)
+				{
+
+					read(file, &wow, 1);	
+					i --;
+					lseek(file, i, SEEK_SET);
+				
+
+					if(wow == '\n')
+					{
+						counter ++;
+					}
+
+					characters++;
+				}
+
+				if (counter >= 10)
+				{
+					characters = characters - 2;
+					char buffer[characters];
+	    			lseek(file, 2, SEEK_CUR);
+			    	read(file, buffer, characters);
+			    	write(1, buffer, characters);
+				}
+
+				else
+				{
+					characters = characters - 1;
+					char buffer[characters];
+					lseek(file, 0, SEEK_SET);
+				    read(file, buffer, characters);
+				    write(1, buffer, characters);
+				}
+				close(file);
+				if(f != argc - 1)
+				{
+					write(1, "\n", 1);
+				}
+			}
+		}
 	}
 
-	else
+	else if(argc == 2)
 	{
 		int file = 0;
 		file = open(argv[1], O_RDONLY);
@@ -48,11 +117,10 @@ int main(int argc, char *argv[])
 				characters++;
 			}
 
-			characters = characters - 2;
-			char buffer[characters];
-
 			if (counter >= 10)
 			{
+				characters = characters - 2;
+				char buffer[characters];
     			lseek(file, 2, SEEK_CUR);
 		    	read(file, buffer, characters);
 		    	write(1, buffer, characters);
@@ -60,6 +128,8 @@ int main(int argc, char *argv[])
 
 			else
 			{
+				characters = characters - 1;
+				char buffer[characters];
 				lseek(file, 0, SEEK_SET);
 			    read(file, buffer, characters);
 			    write(1, buffer, characters);
