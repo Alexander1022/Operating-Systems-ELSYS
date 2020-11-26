f19db36652a50f823f10165bc674533e756c1bfc
Alexander <36764735+Alexander1022@users.noreply.github.com>
2020-11-26 20:01:15 +0200

    damn, it's not that easy
    :0

--- tail.c
+++ tail.c
@@ -11,29 +11,285 @@ int main(int argc, char *argv[])
 {
 	if(argc < 2)
 	{
-		write(1, "You have to write something here!", 33);
+	    char wow;
+		size_t n;
+		int counter = 0;
+		int characters = 0;
+		int std_characters = 0;
+		int i = 0;
+		char c;
+		char ch[9999999999];
+		
+		while(read(STDIN_FILENO, &c, 1) > 0)
+		{
+		}
+
+		i = lseek(STDIN_FILENO, 0, SEEK_END);
+
+		while(i != -1 && counter != 10)
+		{
+
+			read(STDIN_FILENO, &wow, 1);	
+			i --;
+			lseek(STDIN_FILENO, i, SEEK_SET);
+				
+
+			if(wow == '\n')
+			{
+				counter ++;
+			}
+
+			characters++;
+		}
+
+		if (counter >= 10)
+		{
+			characters = characters - 2;
+			char buffer[characters];
+	    	lseek(STDIN_FILENO, 2, SEEK_CUR);
+			read(STDIN_FILENO, buffer, characters);
+			write(1, buffer, characters);
+		}
+
+		else
+		{
+			characters = characters - 1;
+			char buffer[characters];
+			lseek(STDIN_FILENO, 0, SEEK_SET);
+			read(STDIN_FILENO, buffer, characters);
+			write(1, buffer, characters);
+		}
 	} 
 
 	else if(argc > 2)
 	{
 		for(int f = 1 ; f < argc ; f++)
 		{
-			int name_count = strlen(argv[f]);
-			write(1, "==> ", 4);
-			write(1, argv[f], name_count);
-			write(1, " <==", 4);
-			write(1, "\n", 1);
+			if(strcmp(argv[f], "-") == 0)
+			{
+				int file = 0;
+				file = open(argv[f], O_RDONLY);
+				int name_count = strlen("standard input");
+				write(1, "==> ", 4);
+				write(1, "standard input", name_count);
+				write(1, " <==", 4);
+				write(1, "\n", 1);
+				char wow;
+				size_t n;
+				int counter = 0;
+				int characters = 0;
+				int i = 0;
+
+				i = lseek(file, 0, SEEK_END);
+
+				while(i != -1 && counter != 10)
+				{
+
+					read(file, &wow, 1);	
+					i --;
+					lseek(file, i, SEEK_SET);
+				
+
+					if(wow == '\n')
+					{
+						counter ++;
+					}
+
+					characters++;
+				}
+
+				if (counter >= 10)
+				{
+					characters = characters - 2;
+					char buffer[characters];
+	    			lseek(file, 2, SEEK_CUR);
+			    	read(file, buffer, characters);
+			    	write(1, buffer, characters);
+				}
+
+				else
+				{
+					characters = characters - 1;
+					char buffer[characters];
+					lseek(file, 0, SEEK_SET);
+				    read(file, buffer, characters);
+				    write(1, buffer, characters);
+				}
+
+				if(f != argc - 1)
+				{
+					write(1, "\n", 1);
+				}
+			}
+
+				else
+				{
+					int file = 0;
+					char ch;
+					file = open(argv[f], O_RDONLY);
+
+					if(file == -1)
+					{
+						int name_count = strlen(argv[f]);
+						write(2, "tail: cannot open '", 19);
+						write(2, argv[f], name_count);
+						perror("' for reading");
+					}
+
+					else if(read(file, &ch, 1) == -1)
+					{
+						int name_count = strlen(argv[f]);
+						write(2, "tail: error reading '", 21);
+						write(2, argv[f], name_count);
+						perror("'");
+					}
+
+					else
+					{
+						int name_count = strlen(argv[f]);
+						write(1, "==> ", 4);
+						write(1, argv[f], name_count);
+						write(1, " <==", 4);
+						write(1, "\n", 1);
+						char wow;
+						size_t n;
+						int counter = 0;
+						int characters = 0;
+						int i = 0;	
+							i = lseek(file, 0, SEEK_END);
+
+							while(i != -1 && counter != 10)
+							{
+
+								read(file, &wow, 1);	
+								i --;
+								lseek(file, i, SEEK_SET);
+							
+
+								if(wow == '\n')
+								{
+									counter ++;
+								}
+
+								characters++;
+							}
+
+							if (counter >= 10)
+							{
+								characters = characters - 2;
+								char buffer[characters];
+				    			lseek(file, 2, SEEK_CUR);
+						    	read(file, buffer, characters);
+						    	write(1, buffer, characters);
+							}
+
+							else
+							{
+								characters = characters - 1;
+								char buffer[characters];
+								lseek(file, 0, SEEK_SET);
+							    read(file, buffer, characters);
+							    write(1, buffer, characters);
+							}
+
+							if(close(file) == -1)
+							{
+								//tail: error reading 'b.txt': Input/output error
+								int name_count = strlen(argv[f]);
+								write(2, "tail: error reading '", 21);
+								write(2, argv[f], name_count);
+								perror("'");
+							}
+
+							if(f != argc - 1)
+							{
+								write(1, "\n", 1);
+							}
+					}
+				}
+		}
+	}
+
+	else if(argc == 2)
+	{
+
+		if(strcmp(argv[1], "-") == 0)
+			{
+				int name_count = strlen("standard input");
+				write(1, "==> ", 4);
+				write(1, "standard input", name_count);
+				write(1, " <==", 4);
+				write(1, "\n", 1);
+				char wow;
+				size_t n;
+				int counter = 0;
+				int characters = 0;
+				int i = 0;
+				int file = 0;
+				file = open(argv[1], O_RDONLY);
+
+				i = lseek(file, 0, SEEK_END);
+
+				while(i != -1 && counter != 10)
+				{
+
+					read(file, &wow, 1);	
+					i --;
+					lseek(file, i, SEEK_SET);
+				
+
+					if(wow == '\n')
+					{
+						counter ++;
+					}
+
+					characters++;
+				}
+
+				if (counter >= 10)
+				{
+					characters = characters - 2;
+					char buffer[characters];
+	    			lseek(file, 2, SEEK_CUR);
+			    	read(file, buffer, characters);
+			    	write(1, buffer, characters);
+				}
+
+				else
+				{
+					characters = characters - 1;
+					char buffer[characters];
+					lseek(file, 0, SEEK_SET);
+				    read(file, buffer, characters);
+				    write(1, buffer, characters);
+				}
+			}
+
+		else
+		{
 			int file = 0;
-			file = open(argv[f], O_RDONLY);
+			file = open(argv[1], O_RDONLY);
 			char wow;
 			size_t n;
 			int counter = 0;
 			int characters = 0;
 			int i = 0;
+			char ch;
 
-			if(file < 0)
+			if(file == -1)
 			{
-				return -1;
+				int name_count = strlen(argv[1]);
+				write(2, "tail: cannot open '", 19);
+				write(2, argv[1], name_count);
+				perror("' for reading");
+			}
+
+			else if(read(file, &ch, 1) == -1)
+			{
+				int name_count = strlen(argv[1]);
+				write(2, "tail: error reading '", 21);
+				write(2, argv[1], name_count);
+				perror("'");
 			}
 
 			else
@@ -73,68 +329,16 @@ int main(int argc, char *argv[])
 				    read(file, buffer, characters);
 				    write(1, buffer, characters);
 				}
-				close(file);
-				if(f != argc - 1)
-				{
-					write(1, "\n", 1);
-				}
 			}
-		}
+
+			if(close(file) == -1)
+			{
+				//tail: error reading 'b.txt': Input/output error
+				int name_count = strlen(argv[1]);
+				write(2, "tail: error reading '", 21);
+				write(2, argv[1], name_count);
+				perror("'");
+			}
 	}
-
-	else if(argc == 2)
-	{
-		int file = 0;
-		file = open(argv[1], O_RDONLY);
-		char wow;
-		size_t n;
-		int counter = 0;
-		int characters = 0;
-		int i = 0;
-
-		if(file < 0)
-		{
-			return -1;
-		}
-
-		else
-		{	
-			i = lseek(file, 0, SEEK_END);
-
-			while(i != -1 && counter != 10)
-			{
-
-				read(file, &wow, 1);	
-				i --;
-				lseek(file, i, SEEK_SET);
-			
-
-				if(wow == '\n')
-				{
-					counter ++;
-				}
-
-				characters++;
-			}
-
-			if (counter >= 10)
-			{
-				characters = characters - 2;
-				char buffer[characters];
-    			lseek(file, 2, SEEK_CUR);
-		    	read(file, buffer, characters);
-		    	write(1, buffer, characters);
-			}
-
-			else
-			{
-				characters = characters - 1;
-				char buffer[characters];
-				lseek(file, 0, SEEK_SET);
-			    read(file, buffer, characters);
-			    write(1, buffer, characters);
-			}
-		}
-		close(file);
 	}
 }
