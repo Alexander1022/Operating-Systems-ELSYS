20bf99c9c126c3c3b5902e4bf99184da0dc4006f
Xander <36764735+Alexander1022@users.noreply.github.com>
2021-03-26 22:03:15 +0200

    450/600
    450/600

--- starcraft.c
+++ starcraft.c
@@ -38,7 +38,8 @@ int minerals = 0;
 
 int *blocks;
 bool *flag_if_the_mineral_is_busy;
-int *size_of_blocks;
+int size_of_blocks;
+int all_minerals;
 
 struct CommandCenter cc;
 
@@ -47,64 +48,54 @@ void *scv(void *args)
     int scv_counter;
     scv_counter = *((int*)args);
 
-    sleep(3);
-
-    for(int i = 0 ; i < *size_of_blocks ; i++)
+    for(int i = 0 ; i < size_of_blocks ; i++)
     {
-        sleep(3);
-
-        while(*(blocks + i) > 0)
+        if(*(blocks + i) > 0)
         {
+            //Придвижване до непразен минерален блок (симулира се време, 3s).
             sleep(3);
-            if(*(blocks + i) > 8)
+
+            //Проверка дали минералния блок е свободен (не се симулира допълнително време).
+            while(*(blocks + i) > 0 && flag_if_the_mineral_is_busy[i] == 0)
             {
-                if(*(flag_if_the_mineral_is_busy + i) == 0)
-                {
-                    pthread_mutex_lock(&lock);
-                    printf("SCV %d is mining from mineral block %d\n", scv_counter, i + 1);
-                    flag_if_the_mineral_is_busy[i] = 1;
-                    blocks[i] = blocks[i] - 8;
-
-                    flag_if_the_mineral_is_busy[i] = 0;
-                    printf("SCV %d is transporting minerals\n", scv_counter);
-                    sleep(2);
-                
-                    printf("SCV %d delivered minerals to the Command center\n", scv_counter);
-                    cc.all_the_scvs_mined = cc.all_the_scvs_mined + 8;
-                    pthread_mutex_unlock(&lock);
-                    
-                }
-
-
-                else
-                {
-                    break;
-                }
-            }
-
-            else if(*(blocks + i) > 0 || *(blocks + i) < 8)
-            {   
-                int value = *(blocks + i);
+                int in_scv = 0;
                 pthread_mutex_lock(&lock);
+
+                //Копаене на минерали (не се симулира допълнително време).
                 printf("SCV %d is mining from mineral block %d\n", scv_counter, i + 1);
+
                 flag_if_the_mineral_is_busy[i] = 1;
-                blocks[i] = 0;
-                
+
+                if(*(blocks + i) >= 8)
+                {
+                    in_scv = 8;
+                    *(blocks + i) = *(blocks + i) - 8;
+                }
+
+                else if(*(blocks + i) < 8)
+                {
+                    in_scv = *(blocks + i);
+                    *(blocks + i) = 0;
+                }
 
                 flag_if_the_mineral_is_busy[i] = 0;
+
+                pthread_mutex_unlock(&lock);
+
+                //Транспортиране на минералите до Командния център (симулира се време, 2s).
+                
                 printf("SCV %d is transporting minerals\n", scv_counter);
                 sleep(2);
-                
+
+                pthread_mutex_lock(&lock);
+
+                //Разтоварване на минералите (не се симулира допълнително време).
                 printf("SCV %d delivered minerals to the Command center\n", scv_counter);
-                cc.all_the_scvs_mined = cc.all_the_scvs_mined + value;
+                cc.all_the_scvs_mined = cc.all_the_scvs_mined + in_scv;
+
                 pthread_mutex_unlock(&lock);
             }
-
-            else
-            {
-                break;
-            }
-        }  
+        }
     }
     pthread_exit(NULL);
 }
@@ -115,25 +106,24 @@ int main(int argc, char *argv[])
     pthread_t *SCV_T = malloc(5 * sizeof(pthread_t));
 
     int l_rc;
-    int all_minerals = 0;
+    all_minerals = 0;
 
     pthread_mutex_init(&lock, NULL);
 
     if(argc < 2)
     {
-        size_of_blocks = malloc(sizeof(int));
-        *size_of_blocks = 2;
+        size_of_blocks = 2;
 
-        blocks = malloc(*size_of_blocks * sizeof(int));
-        flag_if_the_mineral_is_busy = malloc(*size_of_blocks * sizeof(bool));
+        blocks = malloc(size_of_blocks * sizeof(int));
+        flag_if_the_mineral_is_busy = malloc(size_of_blocks * sizeof(bool));
 
-        for(int i = 0 ; i < *size_of_blocks ; i++)
+        for(int i = 0 ; i < size_of_blocks ; i++)
         {
             blocks[i] = 500;
             all_minerals = all_minerals + blocks[i];
         }
 
-        for(int i = 0 ; i < *size_of_blocks ; i++)
+        for(int i = 0 ; i < size_of_blocks ; i++)
         {
             flag_if_the_mineral_is_busy[i] = 0;
         }
@@ -141,22 +131,23 @@ int main(int argc, char *argv[])
 
     else if(argc == 2)
     {
-        size_of_blocks = malloc(sizeof(int));
-        *size_of_blocks = atoi(argv[1]);
+        size_of_blocks = atoi(argv[1]);
 
-        blocks = malloc(*size_of_blocks * sizeof(int));
-        flag_if_the_mineral_is_busy = malloc(*size_of_blocks * sizeof(bool));
+        blocks = malloc(size_of_blocks * sizeof(int));
+        flag_if_the_mineral_is_busy = malloc(size_of_blocks * sizeof(bool));
 
-        for(int i = 0 ; i < *size_of_blocks ; i++)
+        for(int i = 0 ; i < size_of_blocks ; i++)
         {
+            blocks[i] = 500;
             all_minerals = all_minerals + 500;
         }
 
-        for(int i = 0 ; i < *size_of_blocks ; i++)
+        for(int i = 0 ; i < size_of_blocks ; i++)
         {
             flag_if_the_mineral_is_busy[i] = 0;
         }
     }
+
 
     for(int i = 0 ; i < 5 ; i++)
     {
@@ -171,7 +162,9 @@ int main(int argc, char *argv[])
         }
     }
 
-    /*while(cc.soldier_counter < 20)
+    //Обучаването на войник струва 50 минерала и се симулира време за обучение 1s. 
+    //След обучението на войник трябва да се изведе съобщението “You wanna piece of me, boy?”.
+    while(cc.soldier_counter < 20)
     {
         char choice;
         choice = getc(stdin);
@@ -191,7 +184,7 @@ int main(int argc, char *argv[])
                 printf("Not enough minerals.\n");
             }
         }
-    }*/
+    }
 
     for(int i = 0 ; i < 5 ; i++)
     {
@@ -208,7 +201,6 @@ int main(int argc, char *argv[])
     pthread_mutex_destroy(&lock);
     free(blocks);
     free(flag_if_the_mineral_is_busy);
-    free(size_of_blocks);
     
     return 0;
 }
