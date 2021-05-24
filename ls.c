d5c8e4733b89d8416f19c3dd9a78de1e1e3d95c9
Xander <36764735+Alexander1022@users.noreply.github.com>
2021-05-24 16:29:11 +0300

    24.05.21 - ls
    24.05.21 - ls

--- ls.c
+++ ls.c
@@ -1,3 +1,13 @@
+//--------------------------------------------
+// NAME: Alexander Yordanov
+// CLASS: XIb
+// NUMBER: 11
+// PROBLEM: #4
+// FILE NAME: ls.c (unix file name)
+// FILE PURPOSE:
+// имплементиране на ls
+//---------------------------------------------
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -5,111 +15,147 @@
 #include <unistd.h>
 #include <dirent.h>
 #include <string.h>
+#include <errno.h>
 
-int args_line = 0;
+// игртаят ролята на booleans, ако е подаден флаг става 1
+int flag_A = 0;
+int flag_l = 0;
+int flag_R = 0;
 
-char** parse_cmdline(const char* cmdline)
+void typeOfFile(struct stat givenFile)
 {
-    int arguments = 0;
-    char* copy_of_cmdline = strdup(cmdline);
-    char** divided_please = (char**)malloc(1 * sizeof(char*));
-    char* arr = strtok(copy_of_cmdline, " ");
-    int i;
-
-    while(arr != NULL)
+    switch(givenFile.st_mode & S_IFMT)
     {
-        int size = strlen(arr);
-        int index = 0;
-        i = 0;
+        //обикновен файл - ’-’
+        case S_IFREG:
+            printf("- ");
+            break;
+        //директория - ’d’
+        case S_IFDIR:
+            printf("d ");
+            break;
+        //блоково устройство - ’b
+        case S_IFBLK:
+            printf("b ");
+            break;
+        //символно устройство - ’c’
+        case S_IFCHR:
+            printf("c ");
+            break;
+        //сокет - ’s’
+        case S_IFSOCK:
+            printf("s ");
+            break;
+        //символна връзка - ’l’
+        case S_IFLNK:
+            printf("l ");
+            break;
+        //програмен поток - ’p
+        case S_IFIFO:
+            printf("p ");
+            break;
+    }
+}
 
-        while(i != size)
-        {
-            if(arr[i] == '\n')
-            {
-                break;
-            }
+char* plsMakePath(char* a, char* b, char* c)
+{
+    size_t s1 = strlen(a);
+    size_t s2 = strlen(b);
+    size_t s3 = strlen(c);
 
-            else
-            {
-                arr[index++] = arr[i];
-            }
-            
-            i++;
-        }
+    char* path = malloc(s1 + s2 + s3 + 1);
 
-        arr[index] = '\0';
-
-        divided_please[arguments] = arr;
-        arguments ++;
-        args_line ++;
-        divided_please = realloc(divided_please, (arguments + 1) * sizeof(char*));
-        
-        arr = strtok(NULL, " ");
+    if(path)
+    {
+        memcpy(path, a, s1);
+        memcpy(path + s1, b, s2);
+        memcpy(path + s1 + s2, c, s3 + 1);
     }
 
-    divided_please[arguments] = NULL;
-    return divided_please;
+    return path;
+}
+
+int basic(char* d)
+{
+    DIR* folder = opendir(d);
+    struct dirent* entry;
+    struct stat file;
+
+    if(!folder)
+    {
+
+        if(errno == ENOENT)
+        {
+            printf("ls: cannot access %s: No such file or directory\n", d);
+        }
+
+        else
+        {
+            printf("ls: cannot open directory %s : Permission denied\n", d);
+        }
+        return 0;
+    }
+
+    else
+    {
+        while((entry = readdir(folder)) != NULL)
+        {
+            if(entry->d_name[0] != '.')
+            {
+                char* path = plsMakePath(d, "/", entry->d_name);
+                stat(path, &file);
+                typeOfFile(file);
+                printf("%s\n", entry->d_name); 
+            }
+        }
+    }
+
+    closedir(folder);
+    return 0;
 }
 
 int main(int argc, char* argv[])
 {
-    while(1)
+    if(argc == 1)
     {
-        char* input;
-        size_t size = 150;
-        int path_size = 0;
-        int bytes_read;
+        basic(".");
+    }
 
-        input = (char *)malloc(size * sizeof(char));
-        
-        printf("$ ");
-
-        bytes_read = getline(&input, &size, stdin);
-
-        if(bytes_read != -1)
+    else if(argc > 1)
+    {
+        for(int i = 1 ; i < argc ; i++)
         {
-            char** argv_list = parse_cmdline(input);
-
-            /*for(int s = 0 ; s < args_line ; s++)
+            if(argv[i][0] == '-')
             {
-                printf("%s ", argv_list[s]);
-            }*/
-            if(strcmp(argv_list[0], "ls") == 0)
-            {
-                DIR *mydir;
-                struct dirent *myfile;
-                struct stat mystat;
-
-                char cwd[PATH_MAX];
-
-                if (getcwd(cwd, sizeof(cwd)) != NULL) 
+                for(int a = 0; a < strlen(argv[i]) ; a++)
                 {
-                    mydir = opendir(cwd);
-                    while((myfile = readdir(mydir)) != NULL)
+                    if(argv[i][a] == 'A')
                     {
-                        stat(myfile->d_name, &mystat); 
+                       flag_A = 1;
+                    }
 
-                        if(S_ISREG(mystat.st_mode))
-                        {
-                            printf("%s ", myfile->d_name);
-                        }   
+                    else if(argv[i][a] == 'l')
+                    {
+                        flag_l = 1;
+                    }
+
+                    else if(argv[i][a] == 'R')
+                    {
+                        flag_R = 1;
                     }
                 }
             }
-
-            printf("\n");
-            args_line = 0;
-
-            free(input);
-            free(argv_list);
         }
 
-        else if(bytes_read == -1)
+        for(int i = 1 ; i < argc ; i++)
         {
-            free(input);
-            break;
+            if(argv[i][0] != '-')
+            {
+                basic(argv[i]);
+            }
         }
 
+        //printf("flag -A: %d\nflag -l: %d\nflag -R: %d\n", flag_A, flag_l, flag_R);
     }
 
     return 0;
